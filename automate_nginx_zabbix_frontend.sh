049b53c24d45686723d3c16bcfa3d24bc43ce9da
Eduard <eduardtc@gmail.com>
2023-09-29 10:04:30 +0200

    Create automate_nginx_zabbix_frontend.sh

---
+++ automate_nginx_zabbix_frontend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#-----------------------------------------------------------
+#.SYNOPSIS
+#  Get status variables from Azure MySQL
+#.OUTPUTS
+# n/a
+#.NOTES
+#  Author:         eduardcloud
+#  Creation Date:  29-09-2023
+#-----------------------------------------------------------
+
+apt-get update -y && apt-get upgrade -y
+apt-get install -y nginx
